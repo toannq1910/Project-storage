2227abbd04508b973d3069a6fc940cdcbd76c946
toannq1910 <42596538+toannq1910@users.noreply.github.com>
2026-04-21 18:47:34 +0700

    Add files via upload

---
+++ users.js
@@ -0,0 +1,15 @@
+
+const users = [
+  {
+    email: "toannq24@fpt.com",
+    password: "Admin!@#$%2020",
+    name: "Nguyễn Quốc Toản",
+    role: "Presale Staff"
+  },
+  {
+    email: "admin@fpt.com",
+    password: "Admin!@#$%2020",
+    name: "Admin Presale",
+    role: "Administrator"
+  }
+];
